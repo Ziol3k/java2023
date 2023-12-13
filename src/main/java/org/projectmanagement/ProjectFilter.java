cd580da608ea9ea1734c5767b13359cddb7f5c6e
Ziol3k <jan_ziolo@wp.pl>
2023-12-13 15:36:47 +0100

    test01 complite

--- src/main/java/org/projectmanagement/ProjectFilter.java
+++ src/main/java/org/projectmanagement/ProjectFilter.java
@@ -1,15 +1,8 @@
 package org.projectmanagement;
 
 
-// TODO dokończ implementację interfejsu przez dodanie funkcji, która zwraca
-// true jeśli projekt spełnia dany warunek, i false, gdy nie spełnia.
-// Następnie wykorzystaj w metodzie ProjectManager.java.filterProjects(...).
-// Sygnatura funkcji i sam interfejs wymagają wykorzystania gneryków, które tu usunąłem.
-// Zastanów się, czy w intefejsie potrzebne jest ciało funkcji?
-
-public interface ProjectFilter {
-
-    // boolean filter(Project project, ... criteria) { ... }
+public interface ProjectFilter<T> {
+    boolean test(Project project, T criteria);
 }
 
 //
--- src/main/java/org/projectmanagement/ProjectManager.java
+++ src/main/java/org/projectmanagement/ProjectManager.java
@@ -3,6 +3,7 @@ package org.projectmanagement;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class ProjectManager {
     private final List<Project> projects;
@@ -11,41 +12,56 @@ public class ProjectManager {
         this.projects = new ArrayList<>();
     }
 
+
+
     public Project findProjectByName(String name) throws ProjectNotFoundException {
-        // TODO Znajdź projekt po nazwie. W razie braku takiego projektu,
-        // rzuć błedem ProjectNotFoundException (jest już zdefiniowany w kodzie)
+        for (Project project : projects) {
+            if (project.getName().equals(name)) {
+                return project;
+            }
+        }
         throw new ProjectNotFoundException("Project with name " + name + " not found");
     }
 
-    // TODO Funkcja updateProjectDeadline służy do aktualizacji terminu zakończenia określonego projektu.
-    //  Po znalezieniu projektu o podanej nazwie, funkcja ustawia nowy termin zakończenia.
-    //  Następnie, dla każdego zadania w projekcie, które ma status "Open",
-    //  funkcja aktualizuje status zadania na "Updated due to deadline change".
-    //  Dodatkowo, jeśli nowy termin zakończenia projektu jest bliższy niż 7 dni,
-    //  funkcja zwiększa priorytet zadania o 1.
     public void updateProjectDeadline(String projectName, Date newDeadline) throws ProjectNotFoundException {
         Project project = findProjectByName(projectName);
         project.setDeadline(newDeadline);
 
-        // TODO Aktualizacja zadań w zależności od nowego terminu
+        for (Task task : project.getTasks()) {
+            if (task.getStatus().equals("Open")) {
+                task.setStatus("Updated due to deadline change");
 
-        // TODO Zwiększanie priorytetu zadania, gdy zbliża się nowy termin
-        // Mniej niż 7 dni do deadline
-        // Zwiększ priorytet + 1
+                long daysUntilDeadline = TimeUnit.DAYS.convert(newDeadline.getTime()
+                        - new Date().getTime(), TimeUnit.MILLISECONDS);
+                if (daysUntilDeadline < 7) {
+                    task.setPriority(task.getPriority() + 1);
+                }
             }
         }
+    }
+
+
+
 
     public void addProject(Project project) throws ProjectDuplicatedNameException {
-        // TODO Dokończ dodawanie projektu do kolekcji projects.
-        // W przypadku gdy istnieje już taki projekt,
-        // rzuć błędem ProjectDuplicatedNameException (trzeba dokonczyć implementację).
+        for (Project existingProject : projects) {
+            if (existingProject.getName().equals(project.getName())) {
+                throw new ProjectDuplicatedNameException("Project " + project.getName() + " already exists");
+            }
+        }
         projects.add(project);
     }
 
-    // TODO Zdefiniuj funkcję, która zwraca listę projektów, które spełniają pewne kryterium.
-    // Przy braku projektów spełniających kryterium, zwróć pustą kolekcję.
-    // Zobacz w testach, jak używana jest ta funkcja.
-    // UWAGA: będzie potrzebne użycie typu generycznego, co nie jest oddane w tej sygnaturze:
-    // List<Project> filterProjects(ProjectFilter filter, criteria)
+    public <T> List<Project> filterProjects(ProjectFilter<T> filter, T criteria) {
+        List<Project> filteredProjects = new ArrayList<>();
+        for (Project project : projects) {
+            if (filter.test(project, criteria)) {
+                filteredProjects.add(project);
+            }
+        }
+        return filteredProjects;
+    }
+
+
 }
 //
